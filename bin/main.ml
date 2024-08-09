792fbd4858e266d1283f6fddadd96a1aa5c217d4
Kamal Sacranie <kamalsjunkmail@gmail.com>
2024-08-09 18:12:55 +0100

    Tried to check if exprs are recursive
    Can't really seem to find a good way. I have a function which
    allegedly seems fine but after that it doesnt work

--- bin/main.ml
+++ bin/main.ml
@@ -18,6 +18,29 @@ type pos =
   | Identifier
   | Number
   | Objective
+  | AExp
+  | MExp
+  | Term
+  | EnumLit
+  | Enum
+
+let _show_pos = function
+  | Programme -> "Programme"
+  | Definition -> "Definition"
+  | Definitions -> "Definitions"
+  | Alpha -> "Alpha"
+  | Alphanum -> "Alphanum"
+  | Lowercase -> "Lowercase"
+  | Uppercase -> "Uppercase"
+  | Digit -> "Digit"
+  | Identifier -> "Identifier"
+  | Number -> "Number"
+  | Objective -> "Objective"
+  | AExp -> "AExp"
+  | MExp -> "MExp"
+  | Term -> "Term"
+  | EnumLit -> "EnumLit"
+  | Enum -> "Enum"
 
 type temp =
   | RMany : temp list -> temp
@@ -25,6 +48,7 @@ type temp =
   | G : pos list -> temp (* as in "Generator" *)
   | S : temp list -> temp (* as in "Sequence" *)
   | T : string list -> temp (* as in "Terminal" *)
+  | O : temp -> temp
 
 let uppercase_chars =
   int_range ~s:65 ~e:91 |> List.map (char_of_int << string_of_char)
@@ -49,11 +73,58 @@ let hm =
   add hm Digit [ [ T digits ] ];
   add hm Identifier [ [ G [ Alpha ]; RMany [ G [ Alphanum ] ] ] ];
   add hm Number [ [ RSome [ G [ Digit ] ] ] ];
-  add hm Definition [ [ G [ Identifier ]; T [ "=" ]; G [ Number ] ] ];
-  add hm Definitions [ [ RMany [ S [ G [ Definition ]; T [ ";" ] ] ] ] ];
-  add hm Objective [ [ T [ "min"; "max" ]; T [ ":" ]; G [ Number ] ] ];
+  add hm AExp [ [ G [ MExp ] ]; [ G [ MExp ]; T [ " + "; " - " ]; G [ AExp ] ] ];
+  add hm MExp [ [ G [ Term ] ]; [ G [ Term ]; T [ " * "; " / " ]; G [ MExp ] ] ];
+  add hm EnumLit
+    [
+      [ T [ "{ " ]; RSome [ G [ Alphanum ] ]; O (T [ "," ]); T [ " }" ] ];
+      [
+        T [ "{ " ];
+        S
+          [
+            RSome [ G [ Alphanum ] ];
+            RSome [ S [ T [ ", " ]; RSome [ G [ Alphanum ] ] ] ];
+            O (T [ "," ]);
+          ];
+        T [ " }" ];
+      ];
+    ];
+  add hm Enum [ [ G [ Identifier ] ]; [ G [ EnumLit ] ] ];
+  add hm Term
+    [
+      [ G [ Number ] ];
+      [ G [ Identifier ] ];
+      [ G [ Number ] ];
+      [ G [ Identifier ] ];
+      [ S [ T [ "(" ]; G [ AExp ]; T [ ")" ] ] ];
+    ];
+  add hm Definition [ [ G [ Identifier ]; T [ " = " ]; G [ AExp; Enum ] ] ];
+  add hm Definitions [ [ RMany [ S [ G [ Definition ]; T [ ";\n" ] ] ] ] ];
+  add hm Objective [ [ T [ "min"; "max" ]; T [ ": " ]; G [ AExp ] ] ];
   add hm Programme [ [ G [ Definitions ]; G [ Objective ]; T [ ";" ] ] ];
   hm
+
+let get_all_nested_pos r =
+  let rec get_all_nested_pos' r =
+    match r with
+    | [] -> []
+    | RMany rule :: rest -> get_all_nested_pos' rule @ get_all_nested_pos' rest
+    | RSome rule :: rest -> get_all_nested_pos' rule @ get_all_nested_pos' rest
+    | S rules :: rest -> get_all_nested_pos' rules @ get_all_nested_pos' rest
+    | T _ :: rest -> get_all_nested_pos' rest
+    | G pos :: rest -> pos :: get_all_nested_pos' rest
+    | O rule :: rest -> get_all_nested_pos' [ rule ] @ get_all_nested_pos' rest
+  in
+  get_all_nested_pos' (List.concat r) |> List.concat
+
+let _is_recursive pos =
+  let rec loop pos (seen : 'a list) =
+    let rule = Hashtbl.find hm pos in
+    let poss = get_all_nested_pos rule in
+    if List.exists (fun p -> List.exists (( = ) p) seen) poss then true
+    else List.exists (fun a -> loop a (pos :: seen)) poss
+  in
+  loop pos []
 
 let rec idk_yet (part_of_sp : pos) =
   let (pos_parts : temp list list) = Hashtbl.find hm part_of_sp in
@@ -76,6 +147,7 @@ let rec idk_yet (part_of_sp : pos) =
     | G pos -> idk_yet (pos |> sample_random)
     | T term -> term |> sample_random
     | S terms -> List.fold_left acc_f "" terms
+    | O rule -> if Random.int 101 >= 50 then acc_f "" rule else ""
   in
   List.fold_left acc_f "" (sample_random pos_parts)
 
