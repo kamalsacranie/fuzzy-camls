83bb3019ddd39415ed759f824de0f9b34678eae5
Kamal Sacranie <kamalsjunkmail@gmail.com>
2024-08-12 00:31:47 +0100

    Implemented taking the shortest path from the current point
    This allows us to control the depth of our tree so we don't
    generate insanely large expressions

--- bin/main.ml
+++ bin/main.ml
@@ -1,24 +1,8 @@
 let ( << ) f g x = g (f x)
 let string_of_char c : string = String.make 1 c
+let flip f x y = f y x
 
-(* If we ever rewrite:
-   - Make every part of speech have some sort of importance rating and
-     repetition rules
-   - Maybe call them something else than parts of speech...
-   - Make different gramamrs composable
-   - Make every thing just take one "temp" type. Right now, we have some
-     instances where we have to declare an explicit sequence and others where a
-     list implies a sequence, this is bad design
-   - There's just too many ways to have optional things
-   - Add some way to denote that we can surround some rule with whitespace
-*)
-
-let rec int_range ~s ~e =
-  match s - e with 0 -> [] | _ -> s :: int_range ~s:(s + 1) ~e
-
-let possible_lens = int_range ~s:1 ~e:11
-
-type pos =
+type part_of_sp =
   | Programme
   | Definition
   | Definitions
@@ -36,7 +20,8 @@ type pos =
   | EnumLit
   | Enum
   | Array
-  | Unary
+  | MUnary
+  | DUnary
   | MAExp
   | MMExp
   | MTerm
@@ -44,6 +29,48 @@ type pos =
   | Sum
   | Constraint
   | Constraints
+
+type clause =
+  | TRMany : clause -> clause
+  | TG : part_of_sp -> clause (* as in "Generator" *)
+  | TS : clause list -> clause (* as in "Sequence" *)
+  | TT : string -> clause (* as in "Terminal" *)
+  | TO : clause -> clause
+  | TC : clause list -> clause
+
+(* If we ever rewrite:
+   - Make every part of speech have some sort of importance rating and
+     repetition rules
+   - Maybe call them something else than parts of speech...
+   - Make different gramamrs composable
+   - Make every thing just take one "temp" type. Right now, we have some
+     instances where we have to declare an explicit sequence and others where a
+     list implies a sequence, this is bad design
+   - There's just too many ways to have optional things
+   - Add some way to denote that we can surround some rule with whitespace
+   - Maybe make it so that multiple keys can yield the same rule from the hash
+     map
+*)
+
+let rec int_range ~s ~e =
+  match s - e with 0 -> [] | _ -> s :: int_range ~s:(s + 1) ~e
+
+let possible_lens = int_range ~s:0 ~e:10
+let ( => ) r e x = Hashtbl.add x r e
+let ( <|> ) rs r = match rs with TC rs -> TC (rs @ [ r ]) | _ -> TC [ rs; r ]
+let ( <&> ) rs r = match rs with TS rs -> TS (rs @ [ r ]) | _ -> TS [ rs; r ]
+let ( !<.> ) r = TG r
+let ( ?> ) r = TO r
+let many r = TRMany r
+let some r = r <&> TRMany r
+
+let rec rept rule reps =
+  match reps with
+  | 0 -> rule
+  | x when x < 0 -> failwith "can't repeat a rule negative times"
+  | _ -> rule <&> rept rule (reps - 1)
+
+let ( !> ) r = TT r
 
 let _show_pos = function
   | Programme -> "Programme"
@@ -63,7 +90,8 @@ let _show_pos = function
   | EnumLit -> "EnumLit"
   | Enum -> "Enum"
   | Array -> "Array"
-  | Unary -> "Unary"
+  | MUnary -> "MUnary"
+  | DUnary -> "DUnary"
   | MAExp -> "MAExp"
   | MMExp -> "MMExp"
   | MTerm -> "MTerm"
@@ -72,14 +100,6 @@ let _show_pos = function
   | Constraint -> "Constraint"
   | Constraints -> "Constraints"
 
-type temp =
-  | RMany : temp list -> temp
-  | RSome : temp list -> temp
-  | G : pos list -> temp (* as in "Generator" *)
-  | S : temp list -> temp (* as in "Sequence" *)
-  | T : string list -> temp (* as in "Terminal" *)
-  | O : temp -> temp
-
 let uppercase_chars =
   int_range ~s:65 ~e:91 |> List.map (char_of_int << string_of_char)
 
@@ -87,6 +107,7 @@ let lowercase_chars =
   int_range ~s:97 ~e:123 |> List.map (char_of_int << string_of_char)
 
 let digits = int_range ~s:0 ~e:10 |> List.map string_of_int
+let non_zero_digits = int_range ~s:1 ~e:10 |> List.map string_of_int
 
 (** This is likley a place for optimisation in the future as this is the core
     of our programme and we are essentially random accessing an array. There
@@ -94,133 +115,147 @@ let digits = int_range ~s:0 ~e:10 |> List.map string_of_int
 let sample_random l = List.nth l (Random.int (List.length l))
 
 let hm =
-  let open Hashtbl in
-  let alnum_str = RSome [ G [ Alphanum ] ] in
-  let sep_pos_end_opt pos sep = S [ pos; RMany [ S [ sep; pos ] ]; O sep ] in
-  let paren_exp inner = [ S [ T [ "(" ]; G [ inner ]; T [ ")" ] ] ] in
-  let list_like lbend pos sep rbend =
-    [ lbend; sep_pos_end_opt pos sep; rbend ]
+  let open List in
+  let sep_pos_end_opt pos sep =
+    pos <&> many (sep <&> !>" " <&> pos) <&> ?>sep
   in
-  let hm = create 12345 in
-  add hm Lowercase [ [ T lowercase_chars ] ];
-  add hm Uppercase [ [ T uppercase_chars ] ];
-  add hm Alpha [ [ G [ Lowercase ] ]; [ G [ Uppercase ] ] ];
-  add hm Alphanum [ [ G [ Alpha ] ]; [ G [ Digit ] ] ];
-  add hm Digit [ [ T digits ] ];
-  add hm Identifier [ [ G [ Alpha ]; RMany [ G [ Alphanum ] ] ] ];
-  add hm MIdentifier
-    [
-      [ G [ Identifier ] ];
-      [ G [ Uppercase ]; RMany [ S [ T [ "_" ]; G [ Lowercase ] ] ] ];
-    ];
-  add hm Number [ [ RSome [ G [ Digit ] ] ] ];
-  add hm Array
-    [
-      list_like (T [ "[ " ])
-        (G [ Array; DAExp; DAExp; DAExp; DAExp; DAExp; DAExp; DAExp ])
-        (T [ ", " ]) (T [ " ]" ]);
-    ];
-  add hm DAExp
-    [ [ G [ DMExp ] ]; [ G [ DMExp ]; T [ " + "; " - " ]; G [ DAExp ] ] ];
-  add hm DMExp
-    [ [ G [ DTerm ] ]; [ G [ DTerm ]; T [ " * "; " / " ]; G [ DMExp ] ] ];
-  add hm MAExp
-    [ [ G [ MMExp ] ]; [ G [ MMExp ]; T [ " + "; " - " ]; G [ MAExp ] ] ];
-  add hm MMExp
-    [ [ G [ MTerm ] ]; [ G [ MTerm ]; T [ " * "; " / " ]; G [ MMExp ] ] ];
-  add hm EnumLit [ list_like (T [ "{ " ]) alnum_str (T [ ", " ]) (T [ " }" ]) ];
-  add hm Enum [ [ G [ Identifier ] ]; [ G [ EnumLit ] ] ];
-  add hm Unary [ [ T [ "-"; "+" ]; G [ DAExp ] ] ];
-  add hm Sum
-    [
-      [
-        T [ "SUM" ];
-        T [ "{" ];
-        S [ G [ Lowercase ]; T [ " = " ]; G [ Enum ] ];
-        T [ "}" ];
-      ];
-    ];
-  add hm MTerm
-    [ [ G [ Sum ] ]; [ G [ MIdentifier ] ]; [ G [ Number ] ]; paren_exp MAExp ];
-  add hm DTerm
-    [ [ G [ Number ] ]; [ G [ Unary ] ]; [ G [ Identifier ] ]; paren_exp DAExp ];
-  add hm Definition
-    [
-      [
-        G [ Identifier ];
-        T [ " = " ];
-        G [ DAExp; Enum; Array; DAExp; Enum; DAExp ];
-      ];
-    ];
-  add hm Constraint
-    [
-      [
-        G [ MAExp ];
-        T ([ "<="; ">="; "=="; ">"; "<" ] |> List.map (fun s -> " " ^ s ^ " "));
-        G [ MAExp ];
-      ];
-    ];
-  add hm Constraints [ [ RMany [ S [ G [ Constraint ]; T [ ";\n" ] ] ] ] ];
-  add hm Definitions [ [ RMany [ S [ G [ Definition ]; T [ ";\n" ] ] ] ] ];
-  add hm Objective [ [ T [ "min"; "max" ]; T [ ": " ]; G [ MAExp ] ] ];
-  add hm Programme
-    [ [ G [ Definitions ]; G [ Objective ]; T [ ";\n" ]; G [ Constraints ] ] ];
+  let paren_exp inner = !>"(" <&> inner <&> !>")" in
+  let list_like lbend pos sep rbend =
+    lbend <&> sep_pos_end_opt pos sep <&> rbend
+  in
+  let hm = Hashtbl.create 12345 in
+  let choice_of_string_lits ss =
+    ss |> map ( !> ) |> fold_left ( <|> ) (TC [])
+  in
+
+  [
+    Uppercase => (uppercase_chars |> choice_of_string_lits);
+    Lowercase => (lowercase_chars |> choice_of_string_lits);
+    Alpha => (!<.>Lowercase <|> !<.>Uppercase);
+    Digit => (digits |> choice_of_string_lits);
+    Alphanum => (!<.>Alpha <|> !<.>Digit);
+    Number
+    => (non_zero_digits |> choice_of_string_lits
+       <&> many (!<.>Digit <|> (!>"_" <&> rept !<.>Digit 3)));
+    Identifier => (!<.>Alpha <&> some !<.>Alphanum);
+    MIdentifier
+    => (!<.>Identifier
+       <|> (?>(!>"~" <|> !>"`")
+           <&> !<.>Uppercase
+           <&> (!>"_" <&> !<.>Lowercase |> many)));
+    Array => list_like !>"[ " (!<.>Array <|> !<.>DAExp) !>"," !>"[ ";
+    DAExp => (!<.>DMExp <|> (!<.>DMExp <&> (!>" + " <|> !>" - ") <&> !<.>DAExp));
+    DMExp => (!<.>DTerm <|> (!<.>DTerm <&> (!>" * " <|> !>" / ") <&> !<.>DMExp));
+    DTerm
+    => (!<.>Number <|> !<.>Identifier <|> paren_exp !<.>DAExp <|> !<.>DUnary);
+    MAExp => (!<.>MMExp <|> (!<.>MMExp <&> (!>" + " <|> !>" - ") <&> !<.>MAExp));
+    MMExp => (!<.>MTerm <|> (!<.>MTerm <&> (!>" * " <|> !>" / ") <&> !<.>MMExp));
+    MTerm => (!<.>Number <|> !<.>MIdentifier <|> paren_exp !<.>MAExp <|> !<.>Sum);
+    EnumLit => list_like !>"{ " (some !<.>Alphanum) !>"," !>" }";
+    (* TODO: Enum filter *)
+    Enum => (!<.>Identifier <|> !<.>EnumLit);
+    DUnary => (!>"+" <|> !>"-" <&> !<.>DTerm);
+    MUnary => (!>"+" <|> !>"-" <&> !<.>MTerm);
+    Sum
+    => (!>"SUM" <&> !>"{" <&> !<.>Lowercase <&> !>" = " <&> !<.>Enum <&> !>"}"
+       <&> (!<.>MMExp <|> paren_exp !<.>MAExp));
+    Definition => (!<.>Identifier <&> !>" = " <&> !<.>DAExp);
+    Constraint
+    => (!<.>MAExp
+       <&> ([ "<="; ">="; "=="; ">"; "<" ]
+           |> List.map (fun s -> " " ^ s ^ " ")
+           |> choice_of_string_lits)
+       <&> !<.>MAExp);
+    Constraints => many (!<.>Constraint <&> !>";\n");
+    Definitions => many (!<.>Definition <&> !>";\n");
+    Objective
+    => ([ "min"; "max" ] |> choice_of_string_lits <&> !>": " <&> !<.>MAExp);
+    Programme
+    => (!<.>Definitions <&> !<.>Objective <&> !>";\n" <&> !<.>Constraints);
+  ]
+  |> List.iter (fun f -> f hm);
   hm
 
-let get_all_nested_pos r =
-  let rec get_all_nested_pos' r =
-    match r with
-    | [] -> []
-    | RMany rule :: rest -> get_all_nested_pos' rule @ get_all_nested_pos' rest
-    | RSome rule :: rest -> get_all_nested_pos' rule @ get_all_nested_pos' rest
-    | S rules :: rest -> get_all_nested_pos' rules @ get_all_nested_pos' rest
-    | T _ :: rest -> get_all_nested_pos' rest
-    | G pos :: rest -> pos :: get_all_nested_pos' rest
-    | O rule :: rest -> get_all_nested_pos' [ rule ] @ get_all_nested_pos' rest
+let _shortest (clause : clause) =
+  let rec shortest' len seen clause =
+    let s = shortest' (len + 1) in
+    match clause with
+    | TRMany rule -> s seen rule
+    | TG pos ->
+        if List.exists (( = ) pos) seen then Int.max_int
+        else s (pos :: seen) (Hashtbl.find hm pos)
+    | TT _ -> len
+    | TO rule -> s seen rule
+    | TS rules ->
+        let r =
+          rules |> List.map (s seen) |> List.fold_left Int.max Int.min_int
+        in
+        if r = Int.min_int then Int.max_int else r
+    | TC rules ->
+        rules |> List.map (s seen) |> List.fold_left Int.min Int.max_int
   in
-  get_all_nested_pos' (List.concat r) |> List.concat
+  shortest' 0 [] clause
 
-let _is_recursive pos =
+let select_shortest rules =
+  rules |> List.map _shortest |> List.combine rules
+  |> List.sort (fun (_, a) (_, b) -> a - b)
+  |> List.hd |> fst
+
+let rec get_all_nested_pos rule seen =
+  match rule with
+  | TRMany rule -> get_all_nested_pos rule seen
+  | TS rules ->
+      List.concat (List.map (fun r -> get_all_nested_pos r seen) rules)
+  | TT _ -> []
+  | TG pos ->
+      pos
+      ::
+      (if List.exists (( = ) pos) seen then []
+       else get_all_nested_pos (Hashtbl.find hm pos) (pos :: seen))
+  | TO rule -> get_all_nested_pos rule seen
+  | TC rules ->
+      List.concat (List.map (fun r -> get_all_nested_pos r seen) rules)
+
+let _is_recursive part_of_sp =
   let rec loop pos (seen : 'a list) =
     let rule = Hashtbl.find hm pos in
-    let poss = get_all_nested_pos rule in
-    if List.exists (fun p -> List.exists (( = ) p) seen) poss then true
-    else List.exists (fun a -> loop a (pos :: seen)) poss
+    let poss = get_all_nested_pos rule [ pos ] in
+    List.exists
+      (fun p -> List.exists (( = ) p) seen)
+      poss (* better to use a hashmap because of this step *)
+    || List.exists (fun p -> loop p (pos :: seen)) poss
   in
-  loop pos []
+  match part_of_sp with TG pos -> loop pos [ pos ] | _ -> false
 
-let rec idk_yet (part_of_sp : pos) =
-  let (pos_parts : temp list list) = Hashtbl.find hm part_of_sp in
-  let rec acc_f acc b =
-    let rec loop rule reps =
-      match reps with
-      | 0 -> ""
-      | x when x < 0 -> assert false
-      | _ -> acc_f "" (rule |> sample_random) ^ loop rule (reps - 1)
-    in
-    acc
-    ^
-    match b with
-    | RMany rule ->
-        let reps = sample_random (0 :: possible_lens) in
-        loop rule reps
-    | RSome rule ->
-        let reps = sample_random possible_lens in
-        loop rule reps
-    | G pos -> idk_yet (pos |> sample_random)
-    | T term -> term |> sample_random
-    | S terms -> List.fold_left acc_f "" terms
-    | O rule -> if Random.int 101 >= 50 then acc_f "" rule else ""
+let rec generate_source' (clause : clause) depth =
+  let rec loop rule reps =
+    match reps with
+    | 0 -> ""
+    | x when x < 0 -> failwith "can't repeat a rule negative times"
+    | _ -> generate_source' rule (depth + 1) ^ loop rule (reps - 1)
   in
-  let te = ref (sample_random pos_parts) in
-  for _ = 0 to 1 do
-    te :=
-      if List.exists _is_recursive (get_all_nested_pos [ !te ]) then
-        sample_random pos_parts
-      else !te
-  done;
-  List.fold_left acc_f "" !te
+
+  let clause =
+    if depth > 20 && _is_recursive clause then !<.>Number else clause
+  in
+
+  match clause with
+  | TRMany rule -> loop rule (sample_random possible_lens)
+  | TG pos -> generate_source' (Hashtbl.find hm pos) (depth + 1)
+  | TT string_lit -> string_lit
+  | TS rules ->
+      rules
+      |> List.map ((flip generate_source') (depth + 1))
+      |> List.fold_left ( ^ ) ""
+  | TO rule ->
+      if Random.int 101 >= 50 then generate_source' rule (depth + 1) else ""
+  | TC cs ->
+      generate_source'
+        (if depth > 1_000 then select_shortest cs else sample_random cs)
+        (depth + 1)
+
+let generate_source pos = generate_source' (TG pos) 0
 
 let () =
   Random.self_init ();
-  idk_yet Programme |> print_endline
+  generate_source Programme |> print_endline
